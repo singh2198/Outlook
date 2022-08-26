8afe0b49e511c1087f7d0de56a6b2ac58a30f892
singh2198 <8755akash@gmail.com>
2022-08-26 22:45:44 +0530

    day-4 work

---
+++ script/subcription.js
@@ -0,0 +1,26 @@
+document.querySelector("#btn").addEventListener("click",click)
+function click(){
+    let selector=document.getElementsByClassName("sub").value
+    console.log(selector)
+
+    // if(selector=="1 year"){
+    //     console.log("1 year")
+    // }
+    // else if(selector=="2 year"){
+    //     console.log("2 year")
+    // }
+    // else if(selector=="3 year"){
+    //     console.log("3 year")
+    // }
+    // else{
+    //     console.log("Please select")
+    // }
+
+    
+
+    
+
+    
+
+    
+}
